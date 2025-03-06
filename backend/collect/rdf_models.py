ff700f7758b5831671257e283ea799037531b3ae
Julian Gonggrijp <j.gonggrijp@uu.nl>
2025-03-06 21:52:27 +0100

    Write add/remove SPARQL updates for CollectionMembersField (#141 #142)

--- backend/collect/rdf_models.py
+++ backend/collect/rdf_models.py
@@ -7,6 +7,98 @@ from triplestore.rdf_model import RDFModel
 from triplestore.rdf_field import RDFField, RDFUniquePropertyField
 
 
+# SPARQL update queries for modifying the contents of a collection. There is
+# considerable overlap between the add, remove and clear operations, so we
+# extracted the common parts into separate strings.
+
+gc_existing_count = '''
+  graph ?gc {{
+    ?r schema:upvoteCount ?count .
+  }}
+'''
+
+gc_adjusted_count = '''
+  graph ?gc {{
+    ?r schema:upvoteCount ?count_upd .
+  }}
+'''
+
+collection_member = '''
+  graph ?collection {{
+    ?collection rdfs:member ?r .
+  }}
+'''
+
+graphs_values = '''
+  values ?collection {{ <{collection}> }}
+  values ?gc {{ <{gc_graph}> }}
+'''
+
+existing_membership_filter = '''
+  graph ?collection {{{{
+    filter {} exists {{{{ ?collection rdfs:member ?r }}}}
+  }}}}
+'''.format
+
+decrementing_count = '''
+  graph ?gc {{
+    ?r schema:upvoteCount ?count
+  }}
+  bind (?count - 1 as ?count_upd)
+'''
+
+add_records_update = f'''
+delete {{{{
+  {gc_existing_count}
+}}}}
+insert {{{{
+  {gc_adjusted_count}
+  {collection_member}
+}}}}
+where {{{{
+  {graphs_values}
+  values ?r {{{{ {{added_records}} }}}}
+  {existing_membership_filter('not')}
+  graph ?gc {{{{
+    optional {{{{ ?r schema:upvoteCount ?c }}}}
+  }}}}
+  bind (if(bound(?c), ?c, 0) as ?count)
+  bind (?count + 1 as ?count_upd)
+}}}}
+'''.format
+
+remove_records_update = f'''
+delete {{{{
+  {gc_existing_count}
+  {collection_member}
+}}}}
+insert {{{{
+  {gc_adjusted_count}
+}}}}
+where {{{{
+  {graphs_values}
+  values ?r {{{{ {{removed_records}} }}}}
+  {existing_membership_filter('')}
+  {decrementing_count}
+}}}}
+'''.format
+
+clear_records_update = f'''
+delete {{{{
+  {gc_existing_count}
+  {collection_member}
+}}}}
+insert {{{{
+  {gc_adjusted_count}
+}}}}
+where {{{{
+  {graphs_values}
+  {collection_member}
+  {decrementing_count}
+}}}}
+'''.format
+
+
 class CollectionMembersField(RDFField):
     '''
     Field for the records that are contained in an EDPOP collection.
