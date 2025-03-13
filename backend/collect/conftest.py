f9ca65f063c76db5dc447c4fde753988b06d61a0
Julian Gonggrijp <j.gonggrijp@uu.nl>
2025-03-13 18:21:10 +0100

    Merge collection RDFModel test fixtures into conftest (#141 #142)

--- backend/collect/conftest.py
+++ backend/collect/conftest.py
@@ -1,6 +1,12 @@
 import pytest
+
 from django.contrib.auth.models import User
+from rdflib import URIRef
+
 from projects.models import Project
+from projects.rdf_models import RDFProject
+from collect.rdf_models import EDPOPCollection
+from collect.utils import collection_graph, collection_uri
 
 
 @pytest.fixture()
@@ -18,3 +24,37 @@ def project(db, user):
     )
     project.users.add(user)
     return project
+
+
+@pytest.fixture()
+def rdf_project(project):
+    return RDFProject(project.graph(), project.identifier())
+
+
+def empty_test_collection(rdf_project, name):
+    uri = collection_uri(name)
+    collection = EDPOPCollection(collection_graph(uri), uri)
+    collection.name = name
+    collection.project = rdf_project.uri
+    return collection
+
+
+@pytest.fixture()
+def collection(rdf_project):
+    return empty_test_collection(rdf_project, 'Test collection')
+
+
+@pytest.fixture()
+def collections(rdf_project):
+    return [
+        empty_test_collection(rdf_project, 'Another collection'),
+        empty_test_collection(rdf_project, 'Collections galore'),
+    ]
+
+
+@pytest.fixture()
+def records():
+    return [
+        URIRef('https://example.org/example1'),
+        URIRef('https://example.org/example2')
+    ]
--- backend/collect/rdf_models_test.py
+++ backend/collect/rdf_models_test.py
@@ -1,41 +1,11 @@
 import pytest
-from rdflib import URIRef, RDF, RDFS, Literal
+from rdflib import RDF, RDFS, Literal
 from django.conf import settings
 
 from triplestore.constants import AS, EDPOPCOL
-from projects.models import Project
-from projects.rdf_models import RDFProject
-from collect.rdf_models import EDPOPCollection
-from collect.utils import collection_graph, collection_uri
 from catalogs.triplestore import SCHEMA
 
-
-@pytest.fixture()
-def project(db):
-    project = Project.objects.create(name='test', display_name='Test')
-    rdf_project = RDFProject(project.graph(), project.identifier())
-    return rdf_project
-
-
-def empty_test_collection(project, name):
-    uri = collection_uri(name)
-    collection = EDPOPCollection(collection_graph(uri), uri)
-    collection.name = name
-    collection.project = project.uri
-    return collection
-
-
-@pytest.fixture()
-def collection(project):
-    return empty_test_collection(project, 'Test collection')
-
-
-@pytest.fixture()
-def records():
-    return [
-        URIRef('https://example.org/example1'),
-        URIRef('https://example.org/example2')
-    ]
+from .conftest import empty_test_collection
 
 
 def test_collection_save_empty(collection):
@@ -44,7 +14,7 @@ def test_collection_save_empty(collection):
     assert any(store.triples((collection.uri, RDF.type, EDPOPCOL.Collection)))
 
 
-def test_collection_save_records(project, collection, records):
+def test_collection_save_records(rdf_project, collection, records):
     collection.records = records
     collection.save()
 
@@ -54,7 +24,7 @@ def test_collection_save_records(project, collection, records):
         print(*triple)
 
     assert any(store.triples((collection.uri, RDF.type, EDPOPCOL.Collection)))
-    assert any(store.triples((collection.uri, AS.context, project.uri)))
+    assert any(store.triples((collection.uri, AS.context, rdf_project.uri)))
     assert any(store.triples((collection.uri, RDFS.member, None)))
     assert any(store.triples((None, SCHEMA.upvoteCount, Literal(1))))
 
@@ -64,7 +34,7 @@ def test_collection_save_records(project, collection, records):
     collection.delete()
 
     assert not any(store.triples((collection.uri, RDF.type, EDPOPCOL.Collection)))
-    assert not any(store.triples((collection.uri, AS.context, project.uri)))
+    assert not any(store.triples((collection.uri, AS.context, rdf_project.uri)))
     assert not any(store.triples((collection.uri, RDFS.member, None)))
     assert not any(store.triples((None, SCHEMA.upvoteCount, Literal(1))))
 
@@ -106,9 +76,8 @@ def test_collection_remove_records(collection, records):
     assert any(store.triples((records[1], SCHEMA.upvoteCount, Literal(0))))
 
 
-def test_collection_track_multi_gc(project, collection, records):
-    collection2 = empty_test_collection(project, 'Another collection')
-    collection3 = empty_test_collection(project, 'Collections galore')
+def test_collection_track_multi_gc(rdf_project, collection, collections, records):
+    [collection2, collection3] = collections
     collection.records = records
     collection.save()
     collection2.save()
