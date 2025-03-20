9f8ee5f6c9fec1b03713144e7430071c9e166dee
Julian Gonggrijp <j.gonggrijp@uu.nl>
2025-03-20 19:42:46 +0100

    Remove obsolete import

--- frontend/vre/search/search.model.js
+++ frontend/vre/search/search.model.js
@@ -1,5 +1,5 @@
-import {Record, Records} from '../record/record.model';
-import {JsonLdWithOCCollection} from "../utils/jsonld.model";
+import { Record } from '../record/record.model';
+import { JsonLdWithOCCollection } from "../utils/jsonld.model";
 
 export var SearchResults = JsonLdWithOCCollection.extend({
     url: '/api/catalogs/search/',
