6a4b72a0277ff3b1f3c1c5260509baea3ffd9eee
Tijmen Baarda <tijmenbaarda@gmail.com>
2025-07-29 15:59:04 +0200

    Add view to create a blank record

--- backend/collect/urls.py
+++ backend/collect/urls.py
@@ -1,7 +1,9 @@
 from django.urls import re_path
+from django.urls.conf import path
 
 from . import api
 
 urlpatterns = [
     re_path('collection-records/(?P<collection>.+)/', api.CollectionRecordsView.as_view()),
+    path('blank-record/', api.BlankRecordView.as_view()),
 ]
