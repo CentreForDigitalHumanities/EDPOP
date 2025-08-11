7722ba09f6ae19148a74bb7f44348205f83ca77b
Tijmen Baarda <tijmenbaarda@gmail.com>
2025-08-11 14:11:41 +0200

    Pass annotations to FieldView

--- frontend/vre/field/record.fields.view.js
+++ frontend/vre/field/record.fields.view.js
@@ -1,4 +1,5 @@
 import {AggregateView} from "../core/view";
+import { FilteredCollection } from "../utils/filtered.collection";
 import fieldListTemplate from "./record.fields.view.mustache";
 import {FieldView} from "./field.view";
 
@@ -12,7 +13,23 @@ export var RecordFieldsView = AggregateView.extend({
     },
 
     makeItem: function(model) {
-        var row = new FieldView({model: model});
+        var annotations = model.collection.record.annotations;
+        var field = model.get('key');
+        var annotationsForField = new FilteredCollection(annotations, (annotation) => {
+            var selectField = annotation.get('edpopcol:selectField');
+            var selectOriginalText = annotation.get('edpopcol:selectOriginalText');
+            if (selectField) {
+                var selectFieldId = selectField["@id"];
+                if (selectFieldId === field) {
+                    return !selectOriginalText || selectOriginalText === model.get('value')["edpoprec:originalText"];
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        });
+        var row = new FieldView({model: model, collection: annotationsForField});
         row.on('edit', this.edit, this);
         return row;
     },
