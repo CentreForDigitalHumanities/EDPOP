fa2cc1ef6f34034dc97df9a428e52fcf33415980
Tijmen Baarda <tijmenbaarda@gmail.com>
2025-08-11 16:22:59 +0200

    Outfactor annotation management functionality to annotatable.view.js

--- frontend/vre/field/field.view.js
+++ frontend/vre/field/field.view.js
@@ -4,20 +4,20 @@ import {AnnotationEditView} from "../annotation/annotation.edit.view";
 import {CommentView} from "../annotation/comment.view";
 import _ from "lodash";
 import {Annotation} from "../annotation/annotation.model";
+import {AnnotatableView} from "./annotatable.view";
 
 /**
  * Displays a single model from a FlatFields or FlatAnnotations collection.
  */
-export var FieldView = AggregateView.extend({
+export var FieldView = AnnotatableView.extend({
     tagName: 'tr',
     template: fieldTemplate,
     subview: CommentView,
     container: 'div.annotations',
 
     initialize: function(options) {
-        this.initItems().render().initCollectionEvents();
         this.render().listenTo(this.model, 'change:value', this.renderContainer);
-        this.listenTo(this.collection, 'edit', this.edit);
+        AnnotatableView.prototype.initialize.call(this, options);
     },
 
     makeItem: function(model) {
@@ -45,30 +45,8 @@ export var FieldView = AggregateView.extend({
         return this;
     },
 
-    edit: function(model) {
-        var editTarget = model.clone(),
-            preExisting = this.collection.get(editTarget),
-            newRow;
-        if (preExisting) {
-            var index = this.collection.indexOf(preExisting),
-                oldRow = this.items[index];
-            newRow = new AnnotationEditView({
-                model: preExisting,
-                existing: true,
-            });
-            this.items.splice(index, 1, newRow);
-            oldRow.remove();
-        } else {
-            newRow = new AnnotationEditView({model: editTarget});
-            this.items.push(newRow);
-        }
-        this.placeItems();
-        newRow.on(_.pick(this, ['save', 'cancel', 'trash']), this);
-    },
-
     addComment: function(event) {
         event.preventDefault();
-        console.log(this.model);
         var fieldId = this.model.get('key');
         var fieldContents = this.model.get('value');
         this.edit(new Annotation({
@@ -77,30 +55,5 @@ export var FieldView = AggregateView.extend({
             "edpopcol:selectOriginalText": (fieldContents ? fieldContents['edpoprec:originalText'] : null),
             "oa:motivatedBy": {"@id": "oa:commenting"},
         }));
-    },
-
-    cancel: function(editRow) {
-        var index = _.indexOf(this.items, editRow);
-        editRow.remove();
-        this.items.splice(index, 1);
-        if (editRow.existing) {
-            this.items.splice(index, 0, this.makeItem(editRow.model));
-            this.placeItems();
-        }
-    },
-
-    save: function(editRow) {
-        var model = editRow.model;
-        this.cancel(editRow);
-        this.collection.add(model, {merge: true});
-        model.save();
-    },
-
-    trash: function(editRow) {
-        if (editRow.existing) {
-            this.collection.remove(editRow.model);
-        } else {
-            this.cancel(editRow);
-        }
     },
 });
--- frontend/vre/field/record.annotations.view.js
+++ frontend/vre/field/record.annotations.view.js
@@ -6,8 +6,9 @@ import {AggregateView} from "../core/view";
 import recordAnnotationsTemplate from "./record.annotations.view.mustache";
 import {CommentView} from "../annotation/comment.view";
 import {Annotation} from "../annotation/annotation.model";
+import {AnnotatableView} from "./annotatable.view";
 
-export var RecordAnnotationsView = AggregateView.extend({
+export var RecordAnnotationsView = AnnotatableView.extend({
     template: recordAnnotationsTemplate,
     container: 'tbody',
     subview: CommentView,
@@ -18,36 +19,13 @@ export var RecordAnnotationsView = AggregateView.extend({
     },
 
     initialize: function(options) {
-        this.initItems().render().initCollectionEvents();
-        this.listenTo(this.collection, 'edit', this.edit);
         this.editable = true;  // enables "New field" button
+        AnnotatableView.prototype.initialize.call(this, options);
     },
 
     events: {
         'click button.new-comment': 'editEmpty',
         'click button.new-tag': 'editEmptyTag',
-    },
-
-    edit: function(model) {
-        var project = vreChannel.request('projects:current').get('name'),
-            editTarget = model.clone().set('context', project),
-            preExisting = this.collection.get(editTarget),
-            newRow;
-        if (preExisting) {
-            var index = this.collection.indexOf(preExisting),
-                oldRow = this.items[index];
-            newRow = new AnnotationEditView({
-                model: preExisting,
-                existing: true,
-            });
-            this.items.splice(index, 1, newRow);
-            oldRow.remove();
-        } else {
-            newRow = new AnnotationEditView({model: editTarget});
-            this.items.push(newRow);
-        }
-        this.placeItems();
-        newRow.on(_.pick(this, ['save', 'cancel', 'trash']), this);
     },
 
     editEmpty: function() {
@@ -62,30 +40,5 @@ export var RecordAnnotationsView = AggregateView.extend({
             "oa:hasTarget": this.collection.underlying.target,
             "oa:motivatedBy": {"@id": "oa:tagging"},
         }))
-    },
-
-    cancel: function(editRow) {
-        var index = _.indexOf(this.items, editRow);
-        editRow.remove();
-        this.items.splice(index, 1);
-        if (editRow.existing) {
-            this.items.splice(index, 0, this.makeItem(editRow.model));
-            this.placeItems();
-        }
-    },
-
-    save: function(editRow) {
-        var model = editRow.model;
-        this.cancel(editRow);
-        this.collection.add(model, {merge: true});
-        model.save();
-    },
-
-    trash: function(editRow) {
-        if (editRow.existing) {
-            this.collection.remove(editRow.model);
-        } else {
-            this.cancel(editRow);
-        }
     },
 });
