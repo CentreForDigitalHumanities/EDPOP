fb5cd8ea8807b983c81d73cf53ed868ad9d2db7f
Tijmen Baarda <tijmenbaarda@gmail.com>
2025-08-11 14:13:15 +0200

    Show field annotations and make them editable

--- frontend/vre/field/field.view.js
+++ frontend/vre/field/field.view.js
@@ -1,18 +1,34 @@
-import { View } from '../core/view.js';
+import {AggregateView} from "../core/view";
 import fieldTemplate from './field.view.mustache';
+import {AnnotationEditView} from "../annotation/annotation.edit.view";
+import {CommentView} from "../annotation/comment.view";
+import _ from "lodash";
+import {Annotation} from "../annotation/annotation.model";
 
 /**
  * Displays a single model from a FlatFields or FlatAnnotations collection.
  */
-export var FieldView = View.extend({
+export var FieldView = AggregateView.extend({
     tagName: 'tr',
     template: fieldTemplate,
+    subview: CommentView,
+    container: 'div.annotations',
 
     initialize: function(options) {
-        this.render().listenTo(this.model, 'change:value', this.render);
+        this.initItems().render().initCollectionEvents();
+        this.render().listenTo(this.model, 'change:value', this.renderContainer);
+        this.listenTo(this.collection, 'edit', this.edit);
     },
 
-    render: function() {
+    makeItem: function(model) {
+        return new this.subview({model: model, fieldAnnotation: true});
+    },
+
+    events: {
+        'click a.comment': 'addComment',
+    },
+
+    renderContainer: function() {
         const templateData = {
             field: this.model.get('key'),
         };
@@ -27,5 +43,64 @@ export var FieldView = View.extend({
         }
         this.$el.html(this.template(templateData));
         return this;
+    },
+
+    edit: function(model) {
+        var editTarget = model.clone(),
+            preExisting = this.collection.get(editTarget),
+            newRow;
+        if (preExisting) {
+            var index = this.collection.indexOf(preExisting),
+                oldRow = this.items[index];
+            newRow = new AnnotationEditView({
+                model: preExisting,
+                existing: true,
+            });
+            this.items.splice(index, 1, newRow);
+            oldRow.remove();
+        } else {
+            newRow = new AnnotationEditView({model: editTarget});
+            this.items.push(newRow);
+        }
+        this.placeItems();
+        newRow.on(_.pick(this, ['save', 'cancel', 'trash']), this);
+    },
+
+    addComment: function(event) {
+        event.preventDefault();
+        console.log(this.model);
+        var fieldId = this.model.get('key');
+        var fieldContents = this.model.get('value');
+        this.edit(new Annotation({
+            "oa:hasTarget": this.collection.underlying.target,
+            "edpopcol:selectField": fieldId,
+            "edpopcol:selectOriginalText": (fieldContents ? fieldContents['edpoprec:originalText'] : null),
+            "oa:motivatedBy": {"@id": "oa:commenting"},
+        }));
+    },
+
+    cancel: function(editRow) {
+        var index = _.indexOf(this.items, editRow);
+        editRow.remove();
+        this.items.splice(index, 1);
+        if (editRow.existing) {
+            this.items.splice(index, 0, this.makeItem(editRow.model));
+            this.placeItems();
+        }
+    },
+
+    save: function(editRow) {
+        var model = editRow.model;
+        this.cancel(editRow);
+        this.collection.add(model, {merge: true});
+        model.save();
+    },
+
+    trash: function(editRow) {
+        if (editRow.existing) {
+            this.collection.remove(editRow.model);
+        } else {
+            this.cancel(editRow);
+        }
     },
 });
