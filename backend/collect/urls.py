8fbb20d5fde69e0ed675e269fae6f9689e1d8d33
Julian Gonggrijp <j.gonggrijp@uu.nl>
2025-08-27 21:00:04 +0200

    Properly guard regex paths in collect.urls (#254)

--- backend/collect/urls.py
+++ backend/collect/urls.py
@@ -4,7 +4,7 @@ from . import api
 
 urlpatterns = [
     path('collections/', api.CollectionsView.as_view()),
-    re_path('collections/(?P<collection>.+)/', api.CollectionEditView.as_view()),
-    re_path('collection-records/(?P<collection>.+)/', api.CollectionRecordsView.as_view()),
+    re_path(r'^collections/(?P<collection>.+)/', api.CollectionEditView.as_view()),
+    re_path(r'^collection-records/(?P<collection>.+)/', api.CollectionRecordsView.as_view()),
     path('blank-record/', api.BlankRecordView.as_view()),
 ]
