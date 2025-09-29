ba9badc0391803dd5f273ed8b205061796d0180d
Julian Gonggrijp <j.gonggrijp@uu.nl>
2025-09-29 15:39:39 +0200

    Revert "Temporarily downgrade sync method in frontend Annotation models (#254 #287)"
    This reverts commit 5f11e34eddcc065f9823c03d5ff95dccf0e058c7. The
    workaround is no longer needed because of the changes in previous
    commits. See also #294.

--- frontend/vre/annotation/annotation.model.js
+++ frontend/vre/annotation/annotation.model.js
@@ -1,18 +1,9 @@
-import {
-    getDateTimeLiteral,
-    JsonLdModel,
-    JsonLdNestedCollection,
-    jsonLdSync,
-    priorMethod,
-} from "../utils/jsonld.model";
+import {getDateTimeLiteral, JsonLdModel, JsonLdNestedCollection} from "../utils/jsonld.model";
 import {UserLd} from "../user/user.ld.model";
 import {glossary} from "../utils/glossary";
 
 export var Annotation = JsonLdModel.extend({
     urlRoot: '/api/annotation/',
-    // TODO: remove the next line after adapting the backend so that it can
-    // process JSON-LD as such.
-    sync: priorMethod(JsonLdModel.prototype, 'sync', jsonLdSync),
     getBody: function() {
         return this.get('oa:hasBody');
     },
@@ -57,9 +48,6 @@ export var Annotation = JsonLdModel.extend({
 
 export var Annotations = JsonLdNestedCollection.extend({
     model: Annotation,
-    // TODO: remove the next line after adapting the backend so that it can
-    // process JSON-LD as such.
-    sync: priorMethod(JsonLdModel.prototype, 'sync', jsonLdSync),
     initialize: function(model, options) {
         _.assign(this, _.pick(options, ['target']));
         this.url = `/api/record-annotations/${encodeURIComponent(this.target)}/`;
