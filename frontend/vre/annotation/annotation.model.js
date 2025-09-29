61e75724a06473840357226bec5e208b6b4157e1
Julian Gonggrijp <j.gonggrijp@uu.nl>
2025-09-29 19:16:52 +0200

    Override parse and toJSON methods on frontend Annotation for flattening (#294)

--- frontend/vre/annotation/annotation.model.js
+++ frontend/vre/annotation/annotation.model.js
@@ -1,6 +1,16 @@
+import _ from 'lodash';
+import { parent } from '@uu-cdh/backbone-collection-transformers/src/inheritance.js';
+
 import {getDateTimeLiteral, JsonLdModel, JsonLdNestedCollection} from "../utils/jsonld.model";
 import {UserLd} from "../user/user.ld.model";
 import {glossary} from "../utils/glossary";
+
+// Helper function for removing multiple properties from an object in iteration.
+function stripAttribute(container) {
+    return function(name) {
+        delete container[name];
+    };
+}
 
 export var Annotation = JsonLdModel.extend({
     urlRoot: '/api/annotation/',
@@ -36,6 +46,53 @@ export var Annotation = JsonLdModel.extend({
             console.warn('Unsupported annotation type: ' + motivationId);
             return null;
         }
+    },
+    // Fields that are normally nested in JSON-LD, but get hoisted to the
+    // top-level `attributes` for more convenient access. See the overridden
+    // `parse` and `toJSON` methods below.
+    flatFields: ['motivation', 'glossary', 'oa:hasSource', 'edpopcol:field', 'edpopcol:originalText'],
+    parse: function(response, options) {
+        var anno = parent(Annotation.prototype)
+            .parse.call(this, response, options),
+            flat = {},
+            motivation = anno['oa:motivatedBy'],
+            body = anno['oa:hasBody'],
+            target = anno['oa:hasTarget'];
+        if (motivation) flat.motivation = motivation['@id'];
+        if (body && body['@id']) flat.glossary = body['@id'];
+        if (target) {
+            var source = target['oa:hasSource'],
+                selector = target['oa:hasSelector'];
+            if (source) flat['oa:hasSource'] = source['@id'];
+            if (selector) {
+                var field = selector['edpopcol:field'],
+                    text = selector['edpopcol:originalText'];
+                if (field) flat['edpopcol:field'] = field['@id'];
+                if (text) flat['edpopcol:originalText'] = text;
+            }
+        }
+        return _.assign(flat, anno);
+    },
+    toJSON: function(options) {
+        var jsonld = parent(Annotation.prototype).
+            toJSON.call(this, options),
+            flatMotivation = jsonld.motivation,
+            flatGlossary = jsonld.glossary,
+            target = jsonld['oa:hasTarget'] || {},
+            selector = target['oa:hasSelector'] || {},
+            flatSource = jsonld['oa:hasSource'],
+            flatField = jsonld['edpopcol:field'],
+            flatText = jsonld['edpopcol:originalText'];
+        if (flatField) selector['edpopcol:field'] = {'@id': flatField};
+        if (flatText) selector['edpopcol:originalText'] = flatText;
+        if (flatField || flatText) var flatSelector = true;
+        if (flatSource) target['oa:hasSource'] = {'@id': flatSource};
+        if (flatSelector) target['oa:hasSelector'] = selector;
+        if (flatSource || flatSelector) jsonld['oa:hasTarget'] = target;
+        if (flatGlossary) jsonld['oa:hasBody'] = {'@id': flatGlossary};
+        if (flatMotivation) jsonld['oa:motivatedBy'] = {'@id': flatMotivation};
+        _.each(this.flatFields, stripAttribute(jsonld));
+        return jsonld;
     },
 });
 
