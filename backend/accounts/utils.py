dd491d88be017920caefeb261b190cb2966f1004
Tijmen Baarda <tijmenbaarda@gmail.com>
2025-09-30 17:07:05 +0200

    Add URIRef for special 'import' user

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -8,3 +8,5 @@ RDF_ACCOUNTS_ROOT = settings.RDF_NAMESPACE_ROOT + "accounts/"
 def user_to_uriref(user: User) -> URIRef:
     assert not user.is_anonymous
     return URIRef(RDF_ACCOUNTS_ROOT + user.username)
+
+IMPORT_USER_URIREF = URIRef(RDF_ACCOUNTS_ROOT + "import")
