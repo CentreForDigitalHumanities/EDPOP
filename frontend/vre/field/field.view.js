392903db338ad9e8dc4a4aa29730e9b97b142fa2
Julian Gonggrijp <j.gonggrijp@uu.nl>
2026-08-05 12:10:02 +0200

    squash! WIP rewrite FieldView using new foundations

--- frontend/vre/field/field.view.js
+++ frontend/vre/field/field.view.js
@@ -1,53 +1,24 @@
+import _ from 'lodash';
+
 import { AggregateView } from '../core/view.js';
-import {Annotation} from "../annotation/annotation.model";
+import { Annotation } from '../annotation/annotation.model';
 import { FieldValueView } from './field-value.view.js';
 import fieldRelinkTemplate from './field.relink.options.mustache';
 
 export var FieldView = AggregateView.extend({
-    tagName: 'tr',
-    template: fieldTemplate,
-    subview: CommentView,
-    container: 'div.annotations',
+    tagName: 'tbody',
+    subview: FieldValueView,
 
     initialize: function(options) {
-        this.render().listenTo(this.model, 'change:value', this.render);
-        this.listenTo(this.collection, 'update', this.render);
-        parent(this).initialize.call(this, options);
+        this.collection || this.model && this.collection = this.model.content;
+        this.initItems().render().initCollectionEvents();
+        this.listenTo(this.collection, _.pick(this, [
+            'addValue', 'edit', 'requestRelink', 'discard',
+        ]));
     },
 
-    makeItem: function(model) {
-        return new this.subview({model: model, fieldAnnotation: true});
-    },
+    addValue: function(model, view, event) {
 
-    events: {
-        'click a.comment': 'addEdit',
-    },
-
-    hasEdit: function() {
-        return this.collection.length > 0; // TODO check for just edits
-    },
-
-    renderContainer: function() {
-        const templateData = {
-            field: this.model.get('key'),
-            hasNoEdit: !this.hasEdit(),
-            isEmpty: !this.model.has('value'),
-        };
-        // Check if model is of Field model before using these methods, because
-        // there are some tests relating to old-style annotations that assign
-        // custom models
-        if (typeof this.model.getMainDisplay === 'function') {
-            Object.assign(templateData, {
-                displayText: this.model.getMainDisplay(),
-                fieldInfo: this.model.getFieldInfo(),
-            });
-            var linkedUri = this.model.getLinkedUri();
-            if (linkedUri) {
-                templateData.linkedRecordUri = encodeURIComponent(linkedUri);
-            }
-        }
-        this.$el.html(this.template(templateData));
-        return this;
     },
 
     addEdit: function(event) {
@@ -55,9 +26,9 @@ export var FieldView = AggregateView.extend({
         var fieldId = this.model.get('key');
         var fieldContents = this.model.get('value'); // If undefined, this field did not exist in the original record
         var attributes = {
-            "oa:hasSource": this.collection.underlying.target,
-            "edpopcol:field": fieldId,
-            "motivation": (fieldContents ? "oa:editing" : "oa:describing"),
+            'oa:hasSource': this.collection.underlying.target,
+            'edpopcol:field': fieldId,
+            'motivation': (fieldContents ? 'oa:editing' : 'oa:describing'),
         };
         if (fieldContents) {
             attributes['edpopcol:originalText'] = fieldContents['edpoprec:originalText'];
