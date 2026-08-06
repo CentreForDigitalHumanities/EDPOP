773fdcb539a593ce7a530df505c6df894debc74e
Julian Gonggrijp <j.gonggrijp@uu.nl>
2026-08-06 03:13:10 +0200

    squash! WIP rewrite FieldView using new foundations
    continue adding code

--- frontend/vre/field/field.view.js
+++ frontend/vre/field/field.view.js
@@ -1,7 +1,10 @@
 import _ from 'lodash';
 
+import { vreChannel } from '../radio.js';
 import { AggregateView } from '../core/view.js';
 import { Annotation } from '../annotation/annotation.model';
+import { AnnotationEditView } from '../annotation/annotation.edit.view';
+import { OverlayView } from '../utils/overlay.view.js';
 import { FieldValueView } from './field-value.view.js';
 import fieldRelinkTemplate from './field.relink.options.mustache';
 
@@ -13,12 +16,34 @@ export var FieldView = AggregateView.extend({
         this.collection || this.model && this.collection = this.model.content;
         this.initItems().render().initCollectionEvents();
         this.listenTo(this.collection, _.pick(this, [
-            'addValue', 'edit', 'requestRelink', 'discard',
+            'edit', 'requestRelink', 'discard',
         ]));
     },
 
-    addValue: function(model, view, event) {
+    edit: function(model, view) {
+        this.closeEditor();
+        var project = vreChannel.request('projects:current').id,
+            user = vreChannel.request('user'),
+            original = model.get('original'),
+            originalText = model.get('originalText'),
+            edit = model.get('edit'),
+            correctedText = model.get('correctedText'),
+            annotation;
+        if (
+            edit && user &&
+            edit.getAuthor().getUsername() === user.get('username')
+        ) {
+            annotation = edit.clone();
+        } else {
+            annotation = new Annotation({
+                'context': project,
+                'oa:hasSource': this.model.get('record').id,
+                'edpopcol:field': this.model.id,
+                'motivation': 'oa:editing',
+            });
+            originalText && annotation.set('edpopcol:originalText', originalText);
 
+        }
     },
 
     addEdit: function(event) {
