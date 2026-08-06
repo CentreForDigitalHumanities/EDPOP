9ca71b9ea20133486128b6c160dae8495fd1b80e
Julian Gonggrijp <j.gonggrijp@uu.nl>
2026-08-06 16:59:30 +0200

    squash! WIP rewrite FieldView using new foundations
    remaining logic

--- frontend/vre/field/field.view.js
+++ frontend/vre/field/field.view.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { $ } from 'backbone';
 
 import { vreChannel } from '../radio.js';
 import { AggregateView } from '../core/view.js';
@@ -20,46 +21,114 @@ export var FieldView = AggregateView.extend({
         ]));
     },
 
+    remove: function() {
+        this.cancel().clearRelinker();
+        return FieldView.__super__.remove.call(this);
+    },
+
     edit: function(model, view) {
-        this.closeEditor();
-        var project = vreChannel.request('projects:current').id,
-            user = vreChannel.request('user'),
-            original = model.get('original'),
+        this.cancel();
+        var user = vreChannel.request('user'),
             originalText = model.get('originalText'),
             edit = model.get('edit'),
             correctedText = model.get('correctedText'),
-            annotation;
+            newEdit;
         if (
             edit && user &&
             edit.getAuthor().getUsername() === user.get('username')
         ) {
-            annotation = edit.clone();
+            newEdit = edit.clone();
         } else {
-            annotation = new Annotation({
-                'context': project,
-                'oa:hasSource': this.model.get('record').id,
-                'edpopcol:field': this.model.id,
-                'motivation': 'oa:editing',
-            });
-            originalText && annotation.set('edpopcol:originalText', originalText);
-
+            newEdit = this.makeEdit(originalText);
+            correctedText && newEdit.set('oa:hasBody', correctedText);
         }
+        var editor = new AnnotationEditView({
+            model: newEdit,
+            defaultText: originalText,
+        }).on(_.pick(this, ['save', 'cancel', 'trash']), this);
+        this.editor = new OverlayView({
+            root: this.el,
+            target: view.el,
+            guest: editor,
+        });
+        this.editor.cover();
     },
 
-    addEdit: function(event) {
-        event.preventDefault();
-        var fieldId = this.model.get('key');
-        var fieldContents = this.model.get('value'); // If undefined, this field did not exist in the original record
-        var attributes = {
-            'oa:hasSource': this.collection.underlying.target,
-            'edpopcol:field': fieldId,
-            'motivation': (fieldContents ? 'oa:editing' : 'oa:describing'),
-        };
-        if (fieldContents) {
-            attributes['edpopcol:originalText'] = fieldContents['edpoprec:originalText'];
-            this.edit(new Annotation(attributes), fieldContents['edpoprec:originalText']);
-        } else {
-            this.edit(new Annotation(attributes));
-        }
+    makeEdit: function(originalText) {
+        var newEdit = new Annotation({
+            'context': vreChannel.request('projects:current').id,
+            'oa:hasSource': this.model.get('record').id,
+            'edpopcol:field': this.model.id,
+            'motivation': 'oa:editing',
+        });
+        originalText && newEdit.set('edpopcol:originalText', originalText);
+        return newEdit;
+    },
+
+    cancel: function() {
+        if (!this.editor) return;
+        this.editor.remove();
+        delete this.editor;
+        return this;
+    },
+
+    save: function(editor) {
+        var model = editor.model;
+        this.cancel();
+        model = this.model.annotations.underlying.add(model, {merge: true});
+        model.save();
+    },
+
+    trash: function(editor) {
+        this.dropEdit(editor.model);
+    },
+
+    discard: function(model) {
+        var edit = model.get('edit');
+        if (edit) return this.dropEdit(edit);
+        var originalText = model.get('originalText');
+        if (originalText == null) return;
+        edit = this.makeEdit(originalText);
+        edit.set('marksDeletion', true);
+        this.save({model: edit});
+    },
+
+    dropEdit: function(model) {
+        this.cancel();
+        this.model.annotations.underlying.remove(model);
+    },
+
+    requestRelink: function(model, view, event) {
+        this.clearRelinker();
+        this.relinkPopover = $(event.target).popover({
+            trigger: 'focus',
+            container: 'body',
+            content: fieldRelinkTemplate(this.model),
+            html: true,
+            sanitize: false,
+            placement: 'bottom',
+            title: 'Relink edit to which original value?',
+        });
+        this.relinkPicker = $('body').one(
+            'click',
+            '.relink-option',
+            this.pickRelinkOption.bind(this, model.get('edit')),
+        );
+    },
+
+    clearRelinker: function() {
+        if (!this.relinkPopover) return;
+        this.relinkPopover.dispose();
+        delete this.relinkPopover;
+        this.relinkPicker.off();
+        delete this.relinkPicker;
+        return this;
+    },
+
+    pickRelinkOption: function(edit, event) {
+        if (!edit) return;
+        this.clearRelinker().cancel();
+        edit.set('edpopcol:originalText', event.target.textContent);
+        edit.save();
     },
 });
